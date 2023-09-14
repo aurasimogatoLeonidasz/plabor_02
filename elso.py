7681f853c5777b3b503bb99d3c434f251f09da83
Freesh60 <dodo030304@gmail.com>
2023-09-14 12:35:49 +0200

    elso szinkron

--- elso.py
+++ elso.py
@@ -1,3 +1,18 @@
-print('üdv a fedélzeten')
-print('sas')
+'''print('üdv a \n fedélzeten')
+print('sas')    '''
+print('Szia', 'Lajos' '--')
+print('Mizu')
+
+print('''Ez
+most
+több sorba kerül
+''')
+
+nev= 'Pali'
+print(f'Szia {nev} ')
+print('Hahó {0}'.format(nev))
+
+nev= input('Hogy hívnak:')
+print(f'Szia {nev} ')
+print('Hahó {0}'.format(nev))
 
